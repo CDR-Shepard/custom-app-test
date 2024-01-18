9fd4830f8b1943847619d0a06c4deb776a2a98e0
CDR-Shepard <56300999+CDR-Shepard@users.noreply.github.com>
2024-01-18 11:58:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function connect(crm) {
-    // Add your connection logic here
-    alert('Connecting to ' + crm);
+    if (crm === 'salesforce') {
+        window.location.href = 'https://fivetran.com/connect-card/setup?redirect_uri=https://dmforce.com/analytics&auth=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkIjp7ImxvZ2luIjp0cnVlLCJ1c2VyIjoiY2hlbWljYWxseV9wcm9mbGlnYWN5IiwiYWNjb3VudCI6InN3YXBwaW5nX3R1Y2tlZCIsImdyb3VwIjoidHJhbnNmZXJyZWRfaW5kZWxpYmxlIiwiY29ubmVjdG9yIjoicGxheW1ha2VyX2VtYmFya2VkIiwibWV0aG9kIjoiUGJmQ2FyZCJ9LCJpYXQiOjE3MDU2MDE1MjB9.DsUTWbuogUWQJ6yWqNuim-HskqZ92GqhewvveS0mjB8&hide_setup_guide=true';
+    } else {
+        // For now, do nothing for other CRMs
+        console.log('Connect function for ' + crm + ' is not implemented yet.');
+    }
 }
